5b44d9f2750f01f0d040744f60cd23a7f668ebe6
Tiago Gomes <up201806658@fe.up.pt>
2022-10-15 19:02:32 +0100

    Add: getRelativeDate boundary value analysis code

--- assignment03/jdotxt/src/test/java/com/todotxt/todotxttouch/util/RelativeDateTest.java
+++ assignment03/jdotxt/src/test/java/com/todotxt/todotxttouch/util/RelativeDateTest.java
@@ -1,11 +1,14 @@
 package com.todotxt.todotxttouch.util;
 
+import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -25,42 +28,55 @@ public class RelativeDateTest {
         assertEquals(result, expectedResult);
     }
 
-    static Stream<Arguments> dateProvider() throws ParseException {
-        Calendar calendar = new GregorianCalendar();
-        Calendar calendar2 = new GregorianCalendar();
+    @Test(expected = NullPointerException.class)
+    public void getRelativeDateNull() {
 
+        RelativeDate.getRelativeDate((Date) null); // Partition #4;
+                                                   // Between Partition #4 and Partition #1 On-point;
+                                                   // Between Partition #4 and Partition #2 On-point;
+                                                   // Between Partition #4 and Partition #3 On-point;
+    }
+
+    static Stream<Arguments> dateProvider() throws ParseException {
         Date now = new Date();
         Date before = new SimpleDateFormat("yyyy/MM/dd").parse("2000/10/1");
         Date after = new SimpleDateFormat("yyyy/MM/dd").parse("3000/10/1");
 
-        calendar.setTime(now);
-        calendar2.setTime(now);
-        String nowString = String.format("%04d/%02d/%02d",
-            calendar2.get(Calendar.YEAR) - calendar.get(Calendar.YEAR),
-            calendar2.get(Calendar.MONTH) - calendar.get(Calendar.MONTH), // months start at 0
-            calendar2.get(Calendar.DAY_OF_MONTH) - calendar.get(Calendar.DAY_OF_MONTH)
-        );
+        Instant nowInstant = Instant.now();
+        Instant before1DayInstant = nowInstant.minus(Duration.ofDays(1));
+        Date before1Day = Date.from(before1DayInstant);
 
+        Instant after1DayInstant = nowInstant.plus(Duration.ofDays(1));
+        Date after1Day = Date.from(after1DayInstant);
 
-        calendar2.setTime(before);
-        String beforeString = String.format("%04d/%02d/%02d",
-            calendar2.get(Calendar.YEAR) - calendar.get(Calendar.YEAR),
-            calendar2.get(Calendar.MONTH) - calendar.get(Calendar.MONTH), // months start at 0
-            calendar2.get(Calendar.DAY_OF_MONTH) - calendar.get(Calendar.DAY_OF_MONTH)
-        );
-
-        calendar2.setTime(after);
-        String afterString = String.format("%04d/%02d/%02d",
-            calendar2.get(Calendar.YEAR) - calendar.get(Calendar.YEAR),
-            calendar2.get(Calendar.MONTH) - calendar.get(Calendar.MONTH), // months start at 0
-            calendar2.get(Calendar.DAY_OF_MONTH) - calendar.get(Calendar.DAY_OF_MONTH)
-        );
-
+        String nowString = getRelativeDateUtil(now, now);
+        String beforeString = getRelativeDateUtil(now, before);
+        String afterString = getRelativeDateUtil(now, after);
+        String before1DayString = getRelativeDateUtil(now, before1Day);
+        String after1DayString = getRelativeDateUtil(now, after1Day);
 
         return Stream.of(
-                arguments(before, beforeString), // partition #1
-                arguments(now, nowString), // partition #2
-                arguments(after, afterString) // partition #3
+                arguments(before, beforeString), // Partition #1; Between Partition #4 and Partition #1 Off-point
+                arguments(now, nowString), // Partition #2; Between Partition #1 and Partition #2 Off-point;
+                                           // Between Partition #2 and Partition #3 On-point;
+                                           // Between Partition #4 and Partition #2 Off-point
+                arguments(after, afterString), // Partition #3; Between Partition #4 and Partition #3 Off-point
+                arguments(before1Day, before1DayString), // Between Partition #1 and Partition #2
+                arguments(after1Day, after1DayString) // Between Partition #2 and Partition #3
+        );
+    }
+
+    static String getRelativeDateUtil(Date now, Date date) {
+        Calendar calendar = new GregorianCalendar();
+        Calendar calendar2 = new GregorianCalendar();
+
+        calendar.setTime(now);
+        calendar2.setTime(date);
+
+        return String.format("%04d/%02d/%02d",
+            calendar2.get(Calendar.YEAR) - calendar.get(Calendar.YEAR),
+            calendar2.get(Calendar.MONTH) - calendar.get(Calendar.MONTH), // months start at 0
+            calendar2.get(Calendar.DAY_OF_MONTH) - calendar.get(Calendar.DAY_OF_MONTH)
         );
     }
 }
