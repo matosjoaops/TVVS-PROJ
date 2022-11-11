7c987caeebbcf4dd29f790942abcc3e8e46f8c8e
Tiago Gomes <up201806658@fe.up.pt>
2022-11-11 19:24:16 +0000

    Add: compareListsTest

--- assignment05/jdotxt/src/test/java/com/todotxt/todotxttouch/task/sorter/SortersTest.java
+++ assignment05/jdotxt/src/test/java/com/todotxt/todotxttouch/task/sorter/SortersTest.java
@@ -1,9 +1,11 @@
 package com.todotxt.todotxttouch.task.sorter;
 
+import com.todotxt.todotxttouch.task.Task;
 import org.junit.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -12,7 +14,7 @@ import static org.junit.Assert.assertEquals;
 public class SortersTest {
 
     @Test
-    public void sortersTest() throws ParseException {
+    public void compareDatesTest() throws ParseException {
         Calendar calendar1 = new GregorianCalendar();
         Calendar calendar2 = new GregorianCalendar();
 
@@ -25,5 +27,29 @@ public class SortersTest {
         assertEquals(Sorters.compareDates(null, null, true), 0);
         assertEquals(Sorters.compareDates(calendar1, null, true), -1);
         assertEquals(Sorters.compareDates(null, calendar1, true), 1);
+    }
+
+    @Test
+    public void compareListsTest() {
+        ArrayList<Integer> integerList1 = new ArrayList<>();
+        integerList1.add(1);
+
+        ArrayList<Integer> integerList2 = new ArrayList<>();
+        integerList2.add(1);
+
+        ArrayList<Integer> integerList3 = new ArrayList<>();
+        integerList3.add(2);
+        integerList3.add(3);
+
+        ArrayList<Integer> integerList4 = new ArrayList<>();
+        integerList4.add(2);
+        integerList4.add(3);
+        integerList4.add(4);
+
+        assertEquals(Sorters.compareLists(integerList1, integerList1), 0);
+        assertEquals(Sorters.compareLists(integerList1, integerList2), 0);
+        assertEquals(Sorters.compareLists(integerList1, integerList3), -1);
+        assertEquals(Sorters.compareLists(integerList3, integerList2), 1);
+        assertEquals(Sorters.compareLists(integerList3, integerList4), -1);
     }
 }
