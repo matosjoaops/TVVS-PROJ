d2c1df01d3b823a039b177ef6c2e62abf9430ba9
Tiago Gomes <up201806658@fe.up.pt>
2022-11-11 17:52:34 +0000

    Update: jdotxtTest

--- assignment05/jdotxt/src/test/java/com/jdotxt/JdotxtTest.java
+++ assignment05/jdotxt/src/test/java/com/jdotxt/JdotxtTest.java
@@ -1,15 +1,39 @@
 package com.jdotxt;
 
 import com.chschmid.jdotxt.Jdotxt;
+import com.chschmid.jdotxt.util.FileModifiedListener;
+import com.todotxt.todotxttouch.task.Task;
+import com.todotxt.todotxttouch.util.TaskIo;
 import org.junit.Test;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class JdotxtTest {
+    private static final String DEFAULTDIR = System.getProperty("user.home") + File.separator + "jdotxt";
+    private static final String TODOPATH = DEFAULTDIR + File.separator + "todo.txt";
+    private static final String DONEPATH = DEFAULTDIR + File.separator + "done.txt";
 
     @Test
     public void jdotxtTest() {
         Jdotxt.main(null);
+
+        File todoFile = new File(TODOPATH);
+        assertTrue(todoFile.exists());
+
+        ArrayList<Task> tasks = new ArrayList<>();
+        tasks.add(new Task(1, "A new task"));
+        Jdotxt.taskBag.store(tasks);
+
+        Jdotxt.storeTodos();
+        FileModifiedListener fileModifiedListener = () -> {
+
+        };
+        Jdotxt.removeFileModifiedListener(fileModifiedListener);
+        Jdotxt.archiveTodos();
 
         // what to test here???
 
