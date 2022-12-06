db4ef14f5ddd0741f880081ecd5bf76db1ea7c34
matosjoaops <matosjoaops@protonmail.com>
2022-12-06 15:02:11 +0000

    Add: relative date tests

--- assignment09/jdotxt/src/test/java/com/todotxt/todotxttouch/util/RelativeDateTest.java
+++ assignment09/jdotxt/src/test/java/com/todotxt/todotxttouch/util/RelativeDateTest.java
@@ -9,6 +9,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -27,13 +29,50 @@ public class RelativeDateTest {
         assertEquals(result, expectedResult);
     }
 
+    @ParameterizedTest
+    @MethodSource("calendarProvider")
+    public void getRelativeDateCalendar(Calendar date1, Calendar date2, String expectedResult) {
+        String result = RelativeDate.getRelativeDate(date1, date2);
+        assertEquals(expectedResult, result);
+    }
+
     @Test(expected = NullPointerException.class)
     public void getRelativeDateNull() {
+
+        RelativeDate date = new RelativeDate();
 
         RelativeDate.getRelativeDate((Date) null); // Partition #4;
                                                    // Between Partition #4 and Partition #1 On-point;
                                                    // Between Partition #4 and Partition #2 On-point;
                                                    // Between Partition #4 and Partition #3 On-point;
+    }
+
+    static Stream<Arguments> calendarProvider() throws ParseException {
+        Calendar today = new GregorianCalendar();
+        today.setTime(new Date());
+
+        Calendar twoMonthsInTheFuture = new GregorianCalendar();
+        LocalDate twoMonthsInTheFutureDate = LocalDate.now().plusMonths(2).plusDays(1);
+        twoMonthsInTheFuture.setTime(Date.from(twoMonthsInTheFutureDate.atStartOfDay(ZoneId.systemDefault()).toInstant()));
+
+        Calendar oneMonthInTheFuture = new GregorianCalendar();
+        LocalDate oneMonthInTheFutureDate = LocalDate.now().plusMonths(1).plusDays(1);
+        oneMonthInTheFuture.setTime(Date.from(oneMonthInTheFutureDate.atStartOfDay(ZoneId.systemDefault()).toInstant()));
+
+        Calendar twoDaysInTheFuture = new GregorianCalendar();
+        LocalDate twoDaysInTheFutureDate = LocalDate.now().plusDays(3);
+        twoDaysInTheFuture.setTime(Date.from(twoDaysInTheFutureDate.atStartOfDay(ZoneId.systemDefault()).toInstant()));
+
+        Calendar oneDayInTheFuture = new GregorianCalendar();
+        LocalDate oneDayInTheFutureDate = LocalDate.now().plusDays(2);
+        oneDayInTheFuture.setTime(Date.from(oneDayInTheFutureDate.atStartOfDay(ZoneId.systemDefault()).toInstant()));
+
+        return Stream.of(
+                Arguments.of(twoMonthsInTheFuture, today, "2 months ago"),
+                Arguments.of(oneMonthInTheFuture, today, "1 month ago"),
+                Arguments.of(twoDaysInTheFuture, today, "2 days ago"),
+                Arguments.of(oneDayInTheFuture, today, "1 day ago")
+        );
     }
 
     static Stream<Arguments> dateProvider() throws ParseException {
